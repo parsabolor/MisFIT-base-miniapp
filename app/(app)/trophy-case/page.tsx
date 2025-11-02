ac7616f4e9c23bf47007a4fc7e362129d30146fd
parsabolor <133148107+parsabolor@users.noreply.github.com>
2025-11-02 22:12:32 +0330

    Update page.tsx

--- app/(app)/trophy-case/page.tsx
+++ app/(app)/trophy-case/page.tsx
@@ -1,5 +1,8 @@
 'use client'
-export  const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
+
 import React from 'react'
 
 export default function TrophyCase() {
