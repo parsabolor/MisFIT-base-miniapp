ce210f6ffac14b142c840a491c52c17d5aa6d641
parsabolor <133148107+parsabolor@users.noreply.github.com>
2025-11-02 23:36:20 +0330

    Update layout.tsx

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,10 +1,11 @@
 'use client'
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
-export const fetchCache = 'force-no-store'
 
 import { RootProvider } from '../rootProvider'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return <RootProvider>{children}</RootProvider>
+  return (
+    <RootProvider>
+      {children}
+    </RootProvider>
+  )
 }
