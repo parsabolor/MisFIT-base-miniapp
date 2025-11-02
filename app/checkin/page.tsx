c9da2132be30f67816eab61693d3814c41484b36
parsabolor <133148107+parsabolor@users.noreply.github.com>
2025-11-02 21:23:50 +0330

    Update page.tsx

--- app/checkin/page.tsx
+++ app/checkin/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+export const dynamic = 'force-dynamic'
 import React, { useMemo, useState } from 'react'
 import { useAccount } from 'wagmi'
 import { WorkoutDetailsModal } from '@/components/WorkoutDetailsModal'
