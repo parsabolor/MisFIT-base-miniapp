098189f13eadc6a535827f66c9b06e051c8dbf47
parsabolor <133148107+parsabolor@users.noreply.github.com>
2025-11-02 22:03:04 +0330

    Update layout.tsx

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,11 +1,7 @@
 'use client'
 
-import { RootProvider } from "../rootProvider"
+import { RootProvider } from '../rootProvider'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <RootProvider>
-      <main className="max-w-7xl mx-auto px-4 py-8 space-y-8">{children}</main>
-    </RootProvider>
-  )
+  return <RootProvider>{children}</RootProvider>
 }
