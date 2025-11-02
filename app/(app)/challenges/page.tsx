98572aae1c5f168b02616e0e42f78bfadda95c02
parsabolor <133148107+parsabolor@users.noreply.github.com>
2025-11-02 22:17:16 +0330

    Update page.tsx

--- app/(app)/challenges/page.tsx
+++ app/(app)/challenges/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
 import React, { useMemo, useState } from 'react'
 import { challenges as data } from '@/lib/challenges'
 import { ChallengeCard } from '@/components/ChallengeCard'
