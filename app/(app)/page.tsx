62ab650a1da52a1c88a0ac60fa213686b9bd1a01
parsabolor <133148107+parsabolor@users.noreply.github.com>
2025-11-02 23:25:25 +0330

    Create page.tsx

---
+++ app/(app)/page.tsx
@@ -0,0 +1,9 @@
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
+
+import LandingClient from './LandingClient'
+
+export default function Page() {
+  return <LandingClient />
+}
