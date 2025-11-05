5004ea0935d0bdcd0c66bbf3cb30f0b93995c5a4
parsabolor <133148107+parsabolor@users.noreply.github.com>
2025-11-05 19:18:15 +0330

    Update layout.tsx

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,12 +1,10 @@
-import SiteTabs from '@/components/SiteTabs';
+import SiteTabs from '@/components/SiteTabs'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className="bg-background text-foreground">
-        <SiteTabs />
-        {children}
-      </body>
-    </html>
-  );
+    <>
+      <SiteTabs />
+      {children}
+    </>
+  )
 }
