63be9712592f6e24bc11ca50e8593ca05c6994e0
parsabolor <133148107+parsabolor@users.noreply.github.com>
2025-11-05 19:21:38 +0330

    Update page.tsx

--- app/(app)/leaderboard/page.tsx
+++ app/(app)/leaderboard/page.tsx
@@ -1,7 +1,9 @@
-'use client'
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 export const fetchCache = 'force-no-store'
 
 import LeaderboardClient from './LeaderboardClient'
-export default function Page() { return <LeaderboardClient /> }
+
+export default function Page() {
+  return <LeaderboardClient />
+}
