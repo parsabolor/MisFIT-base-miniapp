07fbfa7f08bea2adce0fa731c899901655ac7f38
parsabolor <133148107+parsabolor@users.noreply.github.com>
2025-11-12 19:26:21 +0330

    Update layout.tsx

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,4 @@
 import SiteTabs from '@/components/SiteTabs'
-import NetworkGuard from '@/components/NetworkGuard'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
